6c548d11eaf352cc62f1ede7b7bc2f9d2fa68681
Josue Henrique <josue21henrique@gmail.com>
2025-02-05 15:13:48 -0300

    ajustando interpretador

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     name: 'API-CANALSUESTE',
     script: 'gunicorn', 
     args: '-w 4 -b 0.0.0.0:3002 run:app', 
+    interpreter: '/usr/local/bin/python3',
     instances: 1,  
     exec_mode: 'fork',  
     env: {
