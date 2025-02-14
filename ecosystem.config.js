025468d93f6c1ae9268f8fe5dc98a476e9f7e02c
Josue Henrique <josue21henrique@gmail.com>
2025-02-14 09:22:44 -0300

    configurado para rodar em sistema LINUX

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,9 @@
-// module.exports = {
-//   apps: [{
-//     name: 'API-CANALSUESTE',
-//     script: 'C:/Users/Infotech/AppData/Local/Programs/Python/Python312/python.exe',
-//     args: 'run.py',
-//     instances: 1,
-//     exec_mode: 'fork',
-//     env: {
-//       FLASK_ENV: 'production',
-//       PORT: 3002
-//     },
-//   }]
-// };
-
 module.exports = {
   apps: [{
     name: 'API-CANALSUESTE',
-    script: 'gunicorn', 
-    args: 'run:app -w 4 -b 0.0.0.0:3002', 
-    interpreter: '/usr/bin/python3', 
+    script: 'gunicorn',
+    args: 'run:app -w 4 -b 0.0.0.0:3002',
+    interpreter: '/usr/bin/python3',
     instances: 1,
     exec_mode: 'fork',
     env: {
