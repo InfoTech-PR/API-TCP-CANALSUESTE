da8235084b021914c0943c946c23f38e0e40e79e
Josue Henrique <josue21henrique@gmail.com>
2025-02-14 08:40:08 -0300

    corrigidno caminho

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,7 @@
 module.exports = {
   apps: [{
     name: 'API-CANALSUESTE',
-    script: 'usr/python3/bin',
+    script: 'usr/bin/python3',
     args: 'run.py',
     instances: 1,
     exec_mode: 'fork',
