a7bbf7901b90e72096039aea041ea67ed41784b7
Josue Henrique <josue21henrique@gmail.com>
2025-02-14 09:21:45 -0300

    configurado para rodar em servidor windows

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app
+from waitress import serve
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=app.config['PORT'])
+    serve(app, host='0.0.0.0', port=3002)
