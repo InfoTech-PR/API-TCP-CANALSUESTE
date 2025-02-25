bedb82e1d15fd8a33e083e402152d7c61b674a48
Josue Henrique <josue21henrique@gmail.com>
2025-02-25 09:11:21 -0300

    consulta_booking ok

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,7 @@ from flask_cors import CORS
 from app.routes.consulta_navio import consulta_navio
 from app.routes.agendamento_expo_cheio import agendar_unidade, consultar_grade, editar_agenda_unidade, deletar_agenda_unidade
 from app.routes.expo_pre_stacking import obter_dados_booking, registrar_prestacking_cheio
-from app.routes.expo_sol_embarque import consulta_due, solicitar_ordem_embarque_due, consulta_movimentacao
-from app.routes.expo_rolagem import rolagem_carga, obter_dados_booking_rolagem
+from app.routes.expo_sol_embarque import consulta_due, solicitar_ordem_embarque_due, consulta_movimentacao, consulta_booking, excluir_conteiner, rolagem_carga
 from app.routes.importacao import bloqueio_nvo, bloqueio_nvo_master, movimentacao_importacao
 
 # logging.basicConfig(level=logging.DEBUG)
@@ -41,8 +40,9 @@ def create_app():
     app.register_blueprint(consulta_due)
     app.register_blueprint(solicitar_ordem_embarque_due)
     app.register_blueprint(consulta_movimentacao)
-    # app.register_blueprint(rolagem_carga)
-    # app.register_blueprint(obter_dados_booking_rolagem)
+    app.register_blueprint(rolagem_carga)
+    app.register_blueprint(excluir_conteiner)
+    app.register_blueprint(consulta_booking)
     app.register_blueprint(bloqueio_nvo)
     app.register_blueprint(bloqueio_nvo_master)
     app.register_blueprint(movimentacao_importacao)
--- app/routes/expo_sol_embarque.py
+++ app/routes/expo_sol_embarque.py
@@ -6,6 +6,9 @@ from config.config import Config
 consulta_due = Blueprint('consulta_due', __name__)
 solicitar_ordem_embarque_due = Blueprint('solicitar_ordem_embarque_due', __name__)
 consulta_movimentacao = Blueprint('consulta_movimentacao', __name__)
+consulta_booking = Blueprint('consulta_booking', __name__)
+excluir_conteiner = Blueprint('excluir_conteiner', __name__)
+rolagem_carga = Blueprint('rolagem_carga', __name__)
 
 @consulta_movimentacao.route('/consulta_movimentacao', methods=['POST'])
 def consulta_movimentacao_endpoint():
@@ -46,3 +49,43 @@ def solicitar_ordem_embarque_due_endpoint():
     conteineres_soap = [{"Conteiner": c["Conteiner"]} for c in conteineres]
     client = get_soap_client(Config.WSDL_URL_EMBARQUE)
     return call_soap_service(client, "SolicitarOrdemEmbarqueDue", NumeroDue=numero_due, AceiteAvarias=aceite_avarias, Conteineres=conteineres_soap)
+
+@consulta_booking.route('/consulta_booking', methods=['POST'])
+def consulta_booking_endpoint():
+    data = request.json
+    booking = data.get('Booking')
+    armador = data.get('Armador')
+    
+    if not booking or not armador:
+        return jsonify({"error": "Os parâmetros 'Booking' e 'Armador' são obrigatórios!"}), 400
+
+    client = get_soap_client(Config.WSDL_URL_EMBARQUE)
+    return call_soap_service(client, "ConsultaBooking", Booking=booking, Armador=armador)
+
+@excluir_conteiner.route('/excluir_conteiner', methods=['POST'])
+def excluir_conteiner_endpoint():
+    data = request.json
+    conteiner = data.get('Conteiner')
+    booking = data.get('Booking')
+    armador = data.get('Armador')
+    navio_indefinido = data.get('NavioIndefinido', False)
+
+    if not conteiner or not booking or not armador:
+        return jsonify({"error": "Os parâmetros 'Conteiner', 'Booking' e 'Armador' são obrigatórios!"}), 400
+
+    client = get_soap_client(Config.WSDL_URL_EMBARQUE)
+    return call_soap_service(client, "RolagemCarga",Conteiner=conteiner, Booking=booking, Armador=armador, NavioIndefinido=navio_indefinido)
+
+@rolagem_carga.route('/rolagem_carga', methods=['POST'])
+def rolagem_carga_endpoint():
+    data = request.json
+    conteiner = data.get('Conteiner')
+    booking = data.get('Booking')
+    armador = data.get('Armador')
+    navio_indefinido = data.get('NavioIndefinido', False)
+
+    if not conteiner or not booking or not armador:
+        return jsonify({"error": "Os parâmetros 'Conteiner', 'Booking' e 'Armador' são obrigatórios!"}), 400
+
+    client = get_soap_client(Config.WSDL_URL_EMBARQUE)
+    return call_soap_service(client, "RolagemCarga",Conteiner=conteiner, Booking=booking, Armador=armador, NavioIndefinido=navio_indefinido)
